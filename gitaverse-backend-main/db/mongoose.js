32d85b7044743614c1799a8f0410544ee7103ab2
Deepanshu <Deepanshukumawat16@gmail.com>
2024-04-17 19:00:51 +0530

    "final"

--- gitaverse-backend-main/db/mongoose.js
+++ gitaverse-backend-main/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-
+// mongodb+srv://deepanshukumawat16:<password>@gitaverse.ob92wwu.mongodb.net/
 mongoose.Promise = global.Promise;
-mongoose.connect("")
+mongoose.connect("mongodb+srv://deepanshukumawat16:deepanshukumawat16@gitaverse.ob92wwu.mongodb.net/new?retryWrites=true&w=majority&appName=gitaverse")
 .then(() => {
   console.log('connected mongo successful')
 })
