aedcbe2d2a26c19bc780c416176bdced526f2846
Deepanshu <Deepanshukumawat16@gmail.com>
2024-05-07 10:03:14 +0530

    FInal

--- gitaverse-frontend-main/src/components/utilities/SignIn/Helpers/SelectField.jsx
+++ gitaverse-frontend-main/src/components/utilities/SignIn/Helpers/SelectField.jsx
@@ -1,16 +1,19 @@
 // SelectField.js
 import React from "react";
 
-const SelectField = ({ label, name, value, options, onChange, required, error }) => {
+const SelectField = ({
+  label,
+  name,
+  value,
+  options,
+  onChange,
+  required,
+  error,
+}) => {
   return (
-    <div className={`${name} ${error ? 'error' : ''}`}>
+    <div className={`${name} ${error ? "error" : ""}`}>
       <label htmlFor={name}></label>
-      <select
-        name={name}
-        value={value}
-        onChange={onChange}
-        required={required}
-      >
+      <select name={name} value={value} onChange={onChange} required={required}>
         {options.map((option) => (
           <option key={option.value} value={option.value}>
             {option.label}
